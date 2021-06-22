5650bb17a8edd1064038d2af87368ece0ce91154
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2021-06-22 11:58:48 +0200

    init parcel boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const principalExtractor = require('@principalstudio/principal-purgecss-extractor');
+
+module.exports = {
+  purge: {
+    content: ['./src/**/*.pug'],
+    extract: {
+      md: content => {
+        return principalExtractor(content);
+      },
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
