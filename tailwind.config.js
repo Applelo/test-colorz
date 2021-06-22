083070dcf10e482e730649b8cbd93061bbdedec9
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2021-06-22 17:01:05 +0200

    work on hero, fix header, add gutter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-const principalExtractor = require('@principalstudio/principal-purgecss-extractor');
+// const principalExtractor = require('@principalstudio/principal-purgecss-extractor');
 
 module.exports = {
-  purge: {
-    content: ['./src/**/*.pug'],
-    extract: {
-      md: content => {
-        return principalExtractor(content);
-      },
-    },
-  },
+  // purge: {
+  //   content: ['./src/**/*.pug'],
+  //   extract: {
+  //     md: content => {
+  //       return principalExtractor(content);
+  //     },
+  //   },
+  // },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
